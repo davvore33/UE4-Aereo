89cf1d766044f12289d306057a7291ac4a6161e9
davvore33 <davvore33@gmail.com>
2017-01-06 10:25:20 +0100

    first commit

---
+++ Source/Aereo/AereoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "AereoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+
+class AEREO_API AAereoGameModeBase : public AGameModeBase {
+    GENERATED_BODY()
+
+
+};
